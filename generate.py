6bc008eca68a726381d70b045627f6287bc1078b
Varun Ramesh <varun@sourcegraph.com>
2014-07-17 22:55:46 -0700

    Added interlinking with hashes.

--- generate.py
+++ generate.py
@@ -5,6 +5,39 @@ import jinja2
 import json
 import argparse
 
+class LinkExtension(markdown.Extension):
+  def __init__(self):
+    pass
+  def extendMarkdown(self, md, md_globals):
+    self.md = md
+
+    # append to end of inline patterns
+    LINK_RE = r'\[\[(.+?)\]\]'
+    linkPattern = LinkPattern(LINK_RE)
+    linkPattern.md = md
+    md.inlinePatterns.add('links', linkPattern, "<not_strong")
+
+class LinkPattern(markdown.inlinepatterns.Pattern):
+    def __init__(self, pattern):
+      super(LinkPattern, self).__init__(pattern)
+
+    def handleMatch(self, m):
+      words = m.group(2).split("|")
+      if len(words) == 2:
+        a = markdown.util.etree.Element('a')
+        a.text = words[0].strip()
+
+        link_parts = words[1].strip().split("#")
+
+        url = link_parts[0] + ".html"
+        if len(link_parts) > 1:
+          url += "#" + link_parts[1].lower()
+
+        a.set('href', url)
+      else:
+          a = ''
+      return a
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Generate a static site that can be uploaded to the server.")
   parser.add_argument("-i", "--inputdir", help="Directory from which to read input files.", type=str, required=True)
@@ -16,7 +49,7 @@ if __name__ == "__main__":
   index_template = template_env.get_template('index.html')
 
   # Markdown processor with extensions
-  md = markdown.Markdown(extensions=['toc', 'meta', 'codehilite', 'extra'])
+  md = markdown.Markdown(extensions=['toc', 'meta', 'codehilite', 'extra', LinkExtension(), 'headerid'])
 
   articles = {}
 
