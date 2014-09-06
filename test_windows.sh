9268b33adc31de127036ae9f07731eeb29761ab4
Varun Ramesh <varun.ramesh@aol.com>
2014-09-05 21:26:05 -0700

    Added windows testing script

---
+++ test_windows.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Create site dir if it does not exist
+mkdir -p output
+
+# Python server
+cd output
+python -m SimpleHTTPServer 3000 &
+cd ..
+
+# Kill python server on exit
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+while true; do
+  echo "Building site..."
+  python generate.py --i "C:\Users\Varun Ramesh\Dropbox\Public\Wiki" --o output/ -r /
+  cp static/* output/
+
+  echo "Waiting for changes..."
+  read -p "Press any key to rebuild... " -n1 -s
+done
